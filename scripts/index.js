9984cd69f3cf9d710b5410635bbe11751a124e27
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-03-13 22:52:02 +0300

    feat:create mouse enter $ leave effect

--- scripts/index.js
+++ scripts/index.js
@@ -28,14 +28,56 @@ submit.addEventListener("click", function(e){
     clearing();
 })
 
-$(document).ready(function(){
-    $(".image1").click(function(){
-    $(".ourWork1").toggle();
+$(document).ready(function () {
+    $(".image1").click(function () {
+        $(".ourWork1").toggle();
     })
-    $(".image2").click(function(){
+    $(".image2").click(function () {
         $(".ourWork2").toggle();
     })
-    $(".image3").click(function(){
-    $(".ourWork3").toggle();
+    $(".image3").click(function () {
+        $(".ourWork3").toggle();
     })
+    // hover effect on portfolio
+    $("#project1").mouseenter(function () {
+        $('.project-heading').filter("#project1-header").show("slow");
+    }).mouseleave(function () {
+        $('#project1-header').hide("slow")
+    });
+
+    $("#project2").mouseenter(function () {
+        $(".project-heading").filter("#project2-header").show('slow');
+    }).mouseleave(function () {
+        $("#project2-header").hide('slow')
+    });
+    $("#project3").mouseenter(function () {
+        $('.project-heading').filter("#project3-header").show("slow");
+    }).mouseleave(function () {
+        $('#project3-header').hide("slow")
+    });
+    $("#project4").mouseenter(function () {
+        $('.project-heading').filter("#project4-header").show("slow");
+    }).mouseleave(function () {
+        $('#project4-header').hide("slow")
+    });
+    $("#project5").mouseenter(function () {
+        $('.project-heading').filter('#project5-header').show("slow");
+    }).mouseleave(function () {
+        $('#project5-header').hide("slow")
+    });
+    $("#project6").mouseenter(function () {
+        $('.project-heading').filter('#project6-header').show("slow");
+    }).mouseleave(function () {
+        $('#project6-header').hide("slow")
+    });
+    $("#project7").mouseenter(function () {
+        $('.project-heading').filter('#project7-header').show("slow");
+    }).mouseleave(function () {
+        $('#project7-header').hide("slow")
+    });
+    $("#project8").mouseenter(function () {
+        $('.project-heading').filter('#project8-header').show("slow");
+    }).mouseleave(function () {
+        $('#project8-header').hide("slow")
+    });
 })
